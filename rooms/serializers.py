58341fd322dd68ce21b160bb3176900576567495
Big J <bigj@Bigui-Macmini.local>
2022-06-10 16:26:07 +0900

    ..

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,30 +1,25 @@
 from rest_framework import serializers
-from users.serializers import UserSerializer
+from users.serializers import RelatedUserSerializer
 from .models import Room
 
-class ReadRoomSerializer(serializers.ModelSerializer):
 
-        user = UserSerializer()
+class RoomSerializer(serializers.ModelSerializer):
 
-        class Meta:
+    user = RelatedUserSerializer()
 
-                model = Room
-                exclude = ("modified",)
+    class Meta:
+        model = Room
+        exclude = ("modified",)
+        read_only_fields = ("user", "id", "created", "updated")
 
-class WriteRoomSerializer(serializers.Serializer) :
-        
-        name = serializers.CharField(max_length=140)
-        address = serializers.CharField(max_length=140)
-        price = serializers.IntegerField(help_text="USD per night")
-        beds = serializers.IntegerField(default=1)
-        lat = serializers.DecimalField(max_digits=10, decimal_places=6)
-        lng = serializers.DecimalField(max_digits=10, decimal_places=6)
-        bedrooms = serializers.IntegerField(default=1)
-        bathrooms = serializers.IntegerField(default=1)
-        check_in = serializers.TimeField(default="00:00:00")
-        check_out = serializers.TimeField(default="00:00:00")
-        instant_book = serializers.BooleanField(default=False)
-
-        def create(self, validated_data):
-
-                return Room.objects.create(**validated_data)
+    def validate(self, data):
+        if self.instance:
+            check_in = data.get("check_in", self.instance.check_in)
+            check_out = data.get("check_out", self.instance.check_out)
+        else:
+            check_in = data.get("check_in")
+            check_out = data.get("check_out")
+        if check_in == check_out:
+            raise serializers.ValidationError(
+                "Not enough time between changes")
+        return data
